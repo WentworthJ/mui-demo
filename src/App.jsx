a0f3e85c4d9252fb4ec0f296537f56ff4a6eec4d
Jason Wentworth <JasonWentworth@student.olympic.edu>
2024-05-21 00:15:57 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VariantButtonGroup from './components/buttonGroup'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        {/* Rendering Buttons */}
+        <VariantButtonGroup buttonOne="CIS155" buttonTwo="CIS255" buttonThree="CIS210"></VariantButtonGroup>
+        {/* Links to external sites with their respective logos */}
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+        <h1>Hello from the main page of the app!</h1>
+        <p>Here are some examples of links to other pages</p>
+        <nav>
+          <ul>
+            <li>
+              <a href="profile">Profile page</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Profile from "./Profile";
+import { Link } from "react-router-dom";
+
+
+const Popeye = () => {
+  return (
+    <>
+      <p>Hi, I am Popeye! I love to eat Spinach!</p>
+      <Link to="/">Click here to go back</Link>
+    </>
+  );
+};
+
+export default Popeye;
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
